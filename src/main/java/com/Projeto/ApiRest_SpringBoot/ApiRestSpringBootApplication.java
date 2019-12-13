be422c7668c587e0d48c5e7a6ea0ac1928869d77
Arthur Vinícius <arthurviny1999@gmail.com>
2019-12-12 22:27:46 -0300

    Adicionando Dependências no pom.xml

--- src/main/java/com/Projeto/ApiRest_SpringBoot/ApiRestSpringBootApplication.java
+++ src/main/java/com/Projeto/ApiRest_SpringBoot/ApiRestSpringBootApplication.java
@@ -1,7 +1,13 @@
 package com.Projeto.ApiRest_SpringBoot;
 
+import java.util.stream.LongStream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.Projeto.ApiRest_SpringBoot.Repositorio.FilmesRepositorio;
 
 @SpringBootApplication
 public class ApiRestSpringBootApplication {
@@ -10,4 +16,16 @@ public class ApiRestSpringBootApplication {
 		SpringApplication.run(ApiRestSpringBootApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(FilmesRepositorio repository) {
+		return args -> {
+			repository.deleteAll();
+			LongStream.range(1, 2).mapToObj(i -> {
+				Filmes f = new Filmes();
+				f.setNameFilme("Testando API");
+				
+				return f;
+			}).map(v -> repository.save(v)).forEach(System.out::println);
+		};
+	}
 }
